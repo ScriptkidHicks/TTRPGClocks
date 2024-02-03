863c86f2b1860c7962511aa82bd9db03238ebd44
Lillith Hicks <tammashicks@gmail.com>
2024-02-03 10:10:43 -0800

    putting together basic layout

---
+++ ttrpg-clocks/src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import faviconimage from "../../public/Images/favicon.png";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <img src="../../public/Images/favicon.png"></img>
+    </main>
+  );
+}
